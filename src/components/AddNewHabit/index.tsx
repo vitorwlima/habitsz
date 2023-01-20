c6f8a1b6e716ebf875638d6688f2dd5c5d202d43
Vitor Windberg <vitorwlima13@gmail.com>
2023-01-20 00:34:28 -0300

    feat: add new habit button style

--- src/components/AddNewHabit/index.tsx
+++ src/components/AddNewHabit/index.tsx
@@ -58,7 +58,7 @@ export const AddNewHabit: React.FC = () => {
   return (
     <>
       <button
-        className="w-fit justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 transition-colors hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+        className="w-fit justify-center rounded-md border-2 border-blue-900 px-4 py-2 text-sm font-medium text-blue-600 transition-colors hover:bg-gray-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2"
         onClick={() => setIsOpen(true)}
       >
         + Add new habit
