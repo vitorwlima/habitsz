8e9a8792ca961d9a12d1963320e7ceff1b93af07
Vitor Windberg <vitorwlima13@gmail.com>
2023-01-26 22:00:16 -0300

    feat: add sidebar links

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import { signOut } from "next-auth/react";
+import { SidebarLink } from "../SidebarLink";
 import { Title } from "../Title";
 
 type Props = {
@@ -18,16 +19,24 @@ export const Sidebar: React.FC<Props> = ({
       <div
         className={`${
           isOpen ? "translate-x-0" : "translate-x-[-100vw]"
-        } absolute z-10 flex h-screen w-screen flex-col justify-between bg-gradient-to-b from-blue-800 to-gray-900 py-8 px-4 transition-all lg:static lg:w-[300px] lg:translate-x-0`}
+        } absolute z-10 flex h-screen w-screen flex-col justify-between bg-gradient-to-b from-blue-800 to-gray-900 py-8 transition-all lg:static lg:w-[300px] lg:translate-x-0`}
       >
-        <header className="flex items-center justify-between lg:justify-center">
-          <button onClick={() => switchIsOpen()} className="lg:hidden">
-            <XMarkIcon className="flex h-8 w-8" />
-          </button>
-          <Title className="text-center" />
-        </header>
-
         <div className="flex flex-col">
+          <header className="flex items-center justify-between px-4 lg:justify-center">
+            <button onClick={() => switchIsOpen()} className="lg:hidden">
+              <XMarkIcon className="flex h-8 w-8" />
+            </button>
+            <Title className="text-center" />
+          </header>
+
+          <nav className="mt-16">
+            <SidebarLink path="dashboard" />
+            <SidebarLink path="habits" />
+            <SidebarLink path="analytics" />
+          </nav>
+        </div>
+
+        <div className="flex flex-col px-4">
           <div className="mb-2 h-1 w-full rounded-full bg-blue-600" />
           <span>
             Hi, <strong>{userName}</strong>!
---
+++ src/components/SidebarLink/index.tsx
@@ -0,0 +1,40 @@
+import {
+  ArrowPathIcon,
+  ChartPieIcon,
+  HomeIcon,
+} from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+type Props = {
+  path: "dashboard" | "habits" | "analytics";
+};
+
+const icons = {
+  dashboard: HomeIcon,
+  habits: ArrowPathIcon,
+  analytics: ChartPieIcon,
+};
+
+export const SidebarLink = ({ path }: Props) => {
+  const { asPath } = useRouter();
+  const Icon = icons[path];
+
+  return (
+    <Link
+      href={path}
+      className={clsx(
+        "flex items-center p-4 text-neutral-100 transition-colors hover:bg-blue-200 hover:bg-opacity-25",
+        {
+          "bg-blue-200 bg-opacity-25": asPath === `/${path}`,
+        }
+      )}
+    >
+      <>
+        <Icon className="mr-4 h-6 w-6" />
+        <span className="capitalize">{path}</span>
+      </>
+    </Link>
+  );
+};
